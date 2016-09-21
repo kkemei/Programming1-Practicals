3f13f5778d9b472ae44b7c74cda37e4f74c9e42a
kkemei <kipngetich.kemei@my.jcu.edu.au>
2016-09-21 22:02:48 +1000

    updating files

--- Prac07/Prac07/guitar.py
+++ Prac07/Prac07/guitar.py
@@ -1,10 +1,13 @@
 class Guitar:
-    def __init__(self, name="", year=0, cost = 0, age = 1922, vintage = False):
+    def __init__(self, name="", year=0, cost = 0):
         self.name = name
         self.year = year
         self.cost = cost
-        self.vintage = vintage
-        self.age =
+        self.age = 2016 - self.year
+        if self.age >= 50:
+            self.vintage = True
+        else:
+            self.vintage = False
 
 
     def get_age(self):
@@ -15,10 +18,10 @@ class Guitar:
     def is_vintage(self):
         if self.age >= 50:
             self.vintage = True
-        self.vintage = False
+        else:
+            self.vintage = False
         return self.vintage
 
 
-
     def __str__(self):
-        return "{} ({}), Worth ${} ({})".format(self.name, self.year, self.cost, self.vintage)
+        return "{} ({}), Worth ${} | ({}) {}".format(self.name, self.year, self.cost, self.vintage, self.age)
--- Prac07/guitarTest.py
+++ Prac07/guitarTest.py
@@ -1,7 +1,21 @@
 from Prac07.guitar import Guitar
 
 def main():
-    test1 = Guitar("Gibson", "1922", "16035.40")
-    print(test1)
+    guitars = []
+    # while True:
+    #     name = input("enter a name")
+    #     if name == '':
+    #         break
+    #     else:
+    #         year = int(input("enter what year it was made"))
+    #         cost = int(input("enter the cost"))
+    # guitars.append(Guitar(name,year,cost))
+    guitars.append(Guitar("Gibson L-5 CES", 1922, 16035.40))
+    guitars.append(Guitar("Line 6 JTV-59", 2010, 1512.9))
+
+    counter = 0
+    for row in guitars:
+        print("Guitar",counter+1,":",guitars[1*counter])
+        counter+=1
 
 main()
