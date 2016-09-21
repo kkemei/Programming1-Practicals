d6c807e5d441fdff9f1633f34268a7dae8e0360b
kkemei <kipngetich.kemei@my.jcu.edu.au>
2016-09-22 09:30:33 +1000

    updating files

--- Prac07/Prac07/guitar.py
+++ Prac07/Prac07/guitar.py
@@ -1,28 +1,26 @@
+def get_age(self, year):
+    self.age = 2016 - self.year
+    return self.age
+
+
+def is_vintage(self):
+    if self.age >= 50:
+        self.vintage = True
+    else:
+        self.vintage = False
+    return self.vintage
+
 class Guitar:
     def __init__(self, name="", year=0, cost = 0):
         self.name = name
         self.year = year
         self.cost = cost
-        self.age = 2016 - self.year
-        if self.age >= 50:
-            self.vintage = True
-        else:
-            self.vintage = False
+        self.age = get_age(self, year)
+        self.vintage = is_vintage(self)
 
-
-    def get_age(self, year):
-        self.age = 2016 - self.year
-
-
-    def is_vintage(self):
-        if self.age >= 50:
-            self.vintage = True
-        else:
-            self.vintage = False
-        return self.vintage
 
 
     def __str__(self):
         # return "{} ({}), Worth ${} | ({}) {}".format(self.name, self.year, self.cost, self.vintage, self.age)
-        return "{:>15} ({}), worth ${:10,.2f} {}".format(self.name, self.year, self.cost, self.vintage)
+        return "{:>15} ({}), worth ${:10,.2f} {} {}".format(self.name, self.year, self.cost, self.vintage, self.age)
 
